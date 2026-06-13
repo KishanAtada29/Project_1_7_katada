c02b7ada31d254b01e5283f47b294b428b55f5a8
Kishan Atada <katada@student.cscc.edu>
2026-06-12 22:16:03 -0400

    Add prefix and manual function, add while loop to validate 'user_choice' for user input, and also add conditional loop

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-subnetmasks = {
+def prefix():
+    subnetmasks = {
     'class_A': '255.0.0.0',
     'class_B': '255.255.0.0',
     'class_C': '255.255.255.0'
-}
+    }
 
-for subnetmask in subnetmasks:
-    print(f'{subnetmask} : {subnetmasks[subnetmask]}') 
+    for subnetmask in subnetmasks:
+        print(f'{subnetmask} : {subnetmasks[subnetmask]}') 
+
+def manual():
+    pass
+
+user_choice = input('Please Enter One of the Choice below\nPrefix\tManual\n')
+user_choice = user_choice.casefold()
+while user_choice != 'prefix' and user_choice != 'manual':
+    print('Invalid..')
+    user_choice = input('Please Enter One of the Choice below\nPrefix\tManual\n')
+    user_choice = user_choice.casefold()
+
+
+if user_choice == 'prefix':
+    prefix()
+elif user_choice == 'manual':
+    manual()
+      
+
+
