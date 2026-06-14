517066696a8b1e0bec7c0319f4eed3b0e1a75480
Kishan Atada <katada@student.cscc.edu>
2026-06-13 22:55:18 -0400

    Add three functions (class_A, class_B, and class_C), update prefix function (subnatemasks dict., and for loop)

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+def class_A():
+    return 24
+def class_B():
+    return 16
+def class_C():
+    return 8
+    
 def prefix():
     subnetmasks = {
-    'class_A': '255.0.0.0',
-    'class_B': '255.255.0.0',
-    'class_C': '255.255.255.0'
+       'class A':{
+           'CIDR': '/8',
+           'Number of usable IPs': (2**class_A()) -2,
+           'Subnetmask': '255.0.0.0'
+       },
+       'class B':{
+           'CIDR': '/16',
+           'Number of usable IPs': (2**class_B()) - 2,
+           'Subnetmask': '255.255.0.0'
+        },
+        'class C':{
+           'CIDR': '/24',
+           'Number of usable IPs': (2**class_C()) - 2,
+           'Subnetmask': '255.255.255.0'
+        }
+
     }
 
-    for subnetmask in subnetmasks:
-        print(f'{subnetmask} : {subnetmasks[subnetmask]}') 
+    for subnetmask, value in subnetmasks.items():
+        print(subnetmask)
+        for label, information in value.items():
+            print(f'{label} : {information}')
+
+        print('------------------------') 
 
 def manual():
     pass
