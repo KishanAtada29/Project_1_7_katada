fe3d55f196c68075c1d1c17379a68f537c8a6cea
Kishan Atada <katada@student.cscc.edu>
2026-06-21 13:35:35 -0400

    updated the classful subet file for better understanding

--- classful_subnet_info.py
+++ classful_subnet_info.py
@@ -1,28 +1,43 @@
 import functions as f
+import subnetmask_calculator as sc
+
 
 def class_A():
-    return 24
+    network_portion = f.single_octet_bits() * 1
+    host_portion = f.single_octet_bits() * 3
+    return host_portion, network_portion
 def class_B():
-    return 16
+    network_portion = f.single_octet_bits() * 2
+    host_portion = f.single_octet_bits() * 2
+    return host_portion, network_portion
 def class_C():
-    return 8
+    network_portion = f.single_octet_bits() * 3
+    host_portion = f.single_octet_bits() * 1
+    return host_portion, network_portion
+
+# In IPv4, 1 represensts network and host represents 0
+# class_x [0] = host side
+# class_x [1] = network side  
 
 def classful_subnet():
+    a = class_A()
+    b = class_B()
+    c = class_C()
     classful_subnet = {
        'class A':{
-           'CIDR': '/8',
-           'Number of usable IPs': (2**class_A()) - f.unusable_IPs(),
-           'Subnetmask': '255.0.0.0'
+           'CIDR': f'/{a[1]}',
+           'Number of usable IPs': (2**a[0]) - f.unusable_IPs(),
+           'Subnet Mask': f'{".".join(map(str,(sc.subnetmask_calculator(a[1]))))}'
        },
        'class B':{
-           'CIDR': '/16',
-           'Number of usable IPs': (2**class_B()) - f.unusable_IPs(),
-           'Subnetmask': '255.255.0.0'
+           'CIDR': f'/{b[1]}',
+           'Number of usable IPs': (2**b[0]) - f.unusable_IPs(),
+           'Subnet Mask': f'{".".join(map(str,(sc.subnetmask_calculator(b[1]))))}'
         },
         'class C':{
-           'CIDR': '/24',
-           'Number of usable IPs': (2**class_C()) - f.unusable_IPs(),
-           'Subnetmask': '255.255.255.0'
+           'CIDR': f'/{c[1]}',
+           'Number of usable IPs': (2**c[0]) - f.unusable_IPs(),
+           'Subnet Mask': f'{".".join(map(str,(sc.subnetmask_calculator(c[1]))))}'
         }
 
     }
